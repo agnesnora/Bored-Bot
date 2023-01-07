9e797c6871329b66cf7802908d6a955e73cdf44b
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-01-07 09:55:14 +0100

    styling

--- index.js
+++ index.js
@@ -5,5 +5,20 @@ document.getElementById("bored-btn").addEventListener("click", function () {
       console.log(data);
 
       document.getElementById("activity").textContent = data.activity;
+      document.getElementById("activity").classList.add("activityHighlight");
     });
 });
+
+/**
+Challenge: 
+
+- Make the styling more exciting once an activity idea comes 
+back from the Bored API
+    - Resources: DOM element "classList" property, uigradients.com, 
+      Google Fonts, color.adobe.com
+    - Some ideas:
+      - Change the title from "BoredBot" to something more exciting!
+      - Change the background to something less drab.
+      - Bonus: Animate something on the screen to move around and add more 
+        excitement to the page
+*/
